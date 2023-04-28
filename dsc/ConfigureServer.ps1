aa61e5285e3a89ef57ed457bb871d31d68f3f1e6
p1johnson <39245689+p1johnson@users.noreply.github.com>
2023-04-28 10:31:41 +0000

    Add MPIO DSC

---
+++ dsc/ConfigureServer.ps1
@@ -0,0 +1,89 @@
+Configuration ConfigureServer {
+
+    Param ()
+
+    #Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, GPRegistryPolicyDsc, NetworkingDsc
+
+    $Interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+
+    Node 'localhost'
+
+    {
+        RegistryPolicyFile DisableServerManagerStart {
+            Key        = 'Software\Policies\Microsoft\Windows\Server\ServerManager'
+            TargetType = 'ComputerConfiguration'
+            ValueName  = 'DoNotOpenAtLogon'
+            ValueData  = 1
+            ValueType  = 'DWORD'
+        }
+
+        RegistryPolicyFile DisableNewNetworkPrompt {
+            Key        = 'System\CurrentControlSet\Control\Network\NewNetworkWindowOff'
+            TargetType = 'ComputerConfiguration'
+            ValueName = '(Default)'
+            ValueType = 'String'
+            Ensure = 'Present'
+        }
+
+        RefreshRegistryPolicy RefreshPolicy {
+            IsSingleInstance = 'Yes'
+            DependsOn        = '[RegistryPolicyFile]DisableServerManagerStart','[RegistryPolicyFile]DisableNewNetworkPrompt'
+        }
+
+        NetConnectionProfile SetPrivateInterface
+        {
+            InterfaceAlias   = $InterfaceAlias
+            NetworkCategory  = 'Private'
+        }
+        
+        FirewallProfile ConfigurePrivateFirewallProfile
+        {
+            Name = 'Private'
+            Enabled = 'False'
+        }
+
+        WindowsFeature Multipath-IO
+        {
+            Name = 'Multipath-IO'
+            Ensure = 'Present'
+        }
+
+        PendingReboot Reboot
+        {
+            Name = 'Reboot'
+            DependsOn = '[WindowsFeature]Multipath-IO'
+        }
+
+        Service MSiSCSI
+        {
+            Name = 'MSiSCSI'
+            StartupType = 'Automatic'
+            State = 'Running'
+        }
+
+        Script MultipathSupport
+        {
+            GetScript = {
+                $returnValue = (Get-MSDSMAutomaticClaimSettings)
+                return $returnValue
+            }
+            TestScript = {
+                $returnValue = $false
+                if ((Get-MSDSMAutomaticClaimSettings).iSCSI) {
+                    if ((Get-MSDSMGlobalDefaultLoadBalancePolicy) -eq 'RR') {
+                        $returnValue = $true
+                    }
+                }
+                return $returnValue
+            }
+            SetScript = {
+                Enable-MSDSMAutomaticClaim -BusType iSCSI
+                Set-MSDSMGlobalDefaultLoadBalancePolicy -Policy RR
+            }
+            DependsOn = '[PendingReboot]Reboot'
+        }
+    }
+
+}
